d81ac11b75d95f4370e5307960be6590247947ac
thirupal2022 <thirupalnaguru@gmail.com>
2026-09-07 22:00:46 +0530

    Build changes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
 import MainLayout from "../layouts/MainLayout";
@@ -11,7 +11,7 @@ import InitiativesPage from "../pages/InitiativesPage";
 import TechHubPage from "../pages/TechHubPage";
 import FuturePage from "../pages/FuturePage";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <MainLayout />,
